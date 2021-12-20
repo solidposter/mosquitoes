cfbc06a0d9ea00b46a286d327e5ac4fe5f6bc432
Tony Sarendal <tony@polarcap.org>
2021-12-20 13:28:04 +0100

    add timestamp when printing errors

--- poller.go
+++ poller.go
@@ -90,6 +90,7 @@ func poller(id int, lifetime int, interval int, url string, reportQ chan<- map[s
 
 			req, err := http.NewRequest("GET", url, nil)
 			if err != nil {
+				fmt.Print(time.Now().Format("20060102 15:04:05.99 "))
 				fmt.Printf("NewRequest error:", err)
 				os.Exit(1)
 			}
@@ -99,6 +100,7 @@ func poller(id int, lifetime int, interval int, url string, reportQ chan<- map[s
 			rStart = time.Now()
 			resp, err := client.Do(req)
 			if err != nil {
+				fmt.Print(time.Now().Format("20060102 15:04:05.99 "))
 				fmt.Println("Do error:", err)
 				rEnd = time.Now()
 				request["error"] = 1
